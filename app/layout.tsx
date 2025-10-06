15b1ea56ca238b3341d67e053b037d1a4cfb0b20
Tarendra Mallick <132709165+Tarendramallick@users.noreply.github.com>
2025-10-06 08:56:52 +0000

    Describe your changes here

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,9 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
+  title: 'PSAtypers',
   description: 'Created with v0',
-  generator: 'v0.app',
+  generator: 'Tarendra-mallick.vercel.app',
 }
 
 export default function RootLayout({
